be9c5e858d1e0945813812e0ec685c629b3fb015
Todd Ross <tross5000@gmail.com>
2013-08-05 01:29:01 -0400

    updated

--- encryption.py
+++ encryption.py
@@ -1,36 +1,16 @@
-import Image, sys
 
-textfile = open(sys.argv[1])
+import Image, sys
+textfile = open(sys.argv[1],'rb')
 textstring = textfile.read()
 textfile.close()
-
 xval = 500
 yval = int(len(textstring)/(xval*4) + 1)
-
-usePass = false
-
-#print yval
-
 im = Image.new('RGBA', (xval,yval), (0,0,0,0))
-
-#print xval, yval
 width = xval
 height = yval
 count = 0
-
-#password stuff
-if sys.argv[3]:
-	usePass = true
-
-#print sys.argv[1]
-
 for y in range (0,height):
-	#count += 1
 	for x in range (0,width):
-		
-		if usePass == true:
-			
-		
 		if count == len(textstring) - 4:
 			im.putpixel((x,y),(ord(textstring[count]),ord(textstring[count + 1]),ord(textstring[count + 2]),ord(textstring[count + 3])))
 			break
@@ -43,31 +23,9 @@ for y in range (0,height):
 		if count == len(textstring) - 1:
 			im.putpixel((x,y),(ord(textstring[count]),0,0,0))
 			break
-		#print "height", y, count
-		#print "width", x, count
 		im.putpixel((x,y),(ord(textstring[count]),ord(textstring[count + 1]),ord(textstring[count + 2]),ord(textstring[count + 3])))
 		count += 4
 im.save(sys.argv[2])
-
-im.show()
-
-
-#print len(textstring)
-
-#print str(ord(val))
-
-#print width
-#print height
-#print pixrgb
-
-
-
-
-
-
-
-
-
 
 
 
